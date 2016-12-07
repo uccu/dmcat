2e8d90995739cecb9ce0d78e17bf14faf30f0a69
uccu <DuanYaFei@jyliMac.local>
2016-12-07 17:54:46 +0800

    update

--- App/Resource/Controller/Api.php
+++ App/Resource/Controller/Api.php
@@ -9,6 +9,8 @@ use AJAX;
 use Request;
 use stdClass;
 
+use App\Resource\Model\ResourceModel as Resource;
+
 class Api extends Controller{
 
 
@@ -18,15 +20,33 @@ class Api extends Controller{
 
     }
 
-    function add(Request $request){
+    function add(Request $request,Resource $resource){
 
         $info = new stdClass;
 
         $token = $request->request('token');
 
         $info->name     = $request->request('name');
-        $info->outlink  = $request->download('outlink');
-        $info->hash     = $request->download('hash');
+        $info->outlink  = $request->request('outlink');
+        $info->hash     = $request->request('hash');
+        $info->ctime    = TIME_NOW; 
+
+        //获取插入的资源ID
+        $id = $resource->set($info)->add()->getStatus();
+
+        if(!$id)AJAX::error('资源上传失败');
+
+        $this->formatResource($id,$info->name);
+
+
+
+        AJAX::success();
+
+    }
+
+    private function formatResource($id,$name){
+
+
 
 
 
