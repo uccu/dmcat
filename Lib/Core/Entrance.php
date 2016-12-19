1b7ed079439e2c3c449c3f1e13c437e31b8df708
uccu <DuanYaFei@jyliMac.local>
2016-12-19 17:37:43 +0800

    修复添加命令行脚本接收传入参数

--- Lib/Core/Entrance.php
+++ Lib/Core/Entrance.php
@@ -57,7 +57,8 @@ require_once LIB_ROOT.'Function/Core.php';
 //定义请求路径
 
 if(!$argc)define('REQUEST_PATH',$_SERVER['PATH_INFO']?substr($_SERVER['PATH_INFO'],1):($_SERVER['REDIRECT_URL']?substr($_SERVER['REDIRECT_URL'],1):''));
-else define('REQUEST_PATH',$argv[0]);
+else define('REQUEST_PATH',$argv[1]);
+
 
 //composer依赖的自动加载
 
