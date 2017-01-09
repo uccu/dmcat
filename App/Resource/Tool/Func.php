1b70dcb1b52743c2d820ef36d96d8e918fd06af7
uccu <DuanYaFei@jyliMac.local>
2017-01-09 18:01:42 +0800

    给func添加注释

--- App/Resource/Tool/Func.php
+++ App/Resource/Tool/Func.php
@@ -6,13 +6,14 @@ use Config;
 
 class Func{
 
-   /**
-    *
-    *  Source : http://www.jquerycn.cn/a_10460
-    *
-    *  Change : pzcat
-    *
-    */
+    /**
+     *
+     * 验证是否是合法的邮箱 
+     * 做了下修改
+     * @source  http://www.jquerycn.cn/a_10460
+     * @return boolean 
+     *
+     */
 
     static function validate_email($email){
 
@@ -27,6 +28,17 @@ class Func{
         }
     }
 
+
+    /**
+     *
+     * 随机字符串
+     * @author pzcat
+     * @param number $count 字符串的长度   
+     * @param boolean $s 是否只输出小写字母
+     * @return string 字符串
+     *
+     */
+
     static function randWord($count = 1,$s = false){
 
         $rand = 'ABCDEFGJIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyz';
@@ -40,25 +52,48 @@ class Func{
 
     }
 
-    const CIPHER = MCRYPT_RIJNDAEL_128;
-    const MODE = MCRYPT_MODE_ECB;  
+    /**
+     *
+     * AES转码/解码
+     * @author pzcat
+     * @param string $str 需要转码/解码的字符串
+     * @param string $key 需要的秘钥
+     * @return string 转码/解码后的字符串
+     *
+     */
 
+     /*AES转码*/
     static public function aes_encode( $str , $key = null){
         if(!$key)$key = Config::get('AES_SECRECT_KEY');
         $key = md5($key);
-        $iv = mcrypt_create_iv(mcrypt_get_iv_size(self::CIPHER,self::MODE),MCRYPT_RAND);  
-        return base64_encode(mcrypt_encrypt(self::CIPHER, $key, $str, self::MODE, $iv));  
+        $iv = mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128,MCRYPT_MODE_ECB),MCRYPT_RAND);  
+        return base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $key, $str, MCRYPT_MODE_ECB, $iv));  
     }  
-
+    /*AES解码*/
     static public function aes_decode( $str , $key = null){
         if(!$key)$key = Config::get('AES_SECRECT_KEY');
         $key = md5($key);
         $str = base64_decode($str);
-        $iv = mcrypt_create_iv(mcrypt_get_iv_size(self::CIPHER,self::MODE),MCRYPT_RAND);  
-        return mcrypt_decrypt(self::CIPHER, $key, $str, self::MODE, $iv);  
+        $iv = mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128,MCRYPT_MODE_ECB),MCRYPT_RAND);  
+        return mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $key, $str, MCRYPT_MODE_ECB, $iv);  
     }  
 
 
+    /**
+     *
+     * 人性化时间转换
+     * @author pzcat
+     * @param number $time 10位数字的时间戳
+     * 
+     * @return string 
+     *      今日
+     *      当小于1m时      xx秒前
+     *      当小于1h时      xx分xx秒前
+     *      当小于1d时      xx时xx分前
+     *      昨日           昨日xx时xx分
+     *      其他           xx-xx-xx
+     *
+     */
 
     static public function time_calculate( $time ){
 
