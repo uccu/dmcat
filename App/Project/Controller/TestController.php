511685c26fb541759246247738c14f2cd5b11a76
uccu <418667631@qq.com>
2017-01-13 20:57:18 +0800

    up

--- App/Project/Controller/TestController.php
+++ App/Project/Controller/TestController.php
@@ -107,7 +107,7 @@ class TestController extends Controller{
 
         global $argc;
         global $argv;
-        //if(!$argc)AJAX::error('请在shell运行');
+        if(!$argc)AJAX::error('请在shell运行');
         
         ignore_user_abort();
         set_time_limit(600);
@@ -121,8 +121,8 @@ class TestController extends Controller{
         // curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
         // curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
         $json = curl_exec($ch);
+        $json=gzdecode($json);
         curl_close($ch);
-        echo $json;die();
         if($json)$json = json_decode($json);
         else{
             echo 'error';
@@ -157,7 +157,12 @@ class TestController extends Controller{
             echo $json;
             curl_close($ch);
 
-            $cache->csave('last_data_bili',$data->aid);
+            $cache->csave('last_data_bili_aid',$data->aid);
+            if($k+1!=$length){
+                $rand = rand(0,floor(600/$length));
+                sleep($rand);
+                // die();
+            }
             
         }
 
@@ -227,50 +232,7 @@ class TestController extends Controller{
 
     }
 
-    function apitest(){
-
-        global $argc;
-        global $argv;
-        if(!$argc)AJAX::error('请在shell运行');
-        
-        ignore_user_abort();
-        set_time_limit(600);
-
-
-        $json = '{
-            "data_id": 469350,
-            "hash_id": "b4dsedb3d024e382ae3e5b56b81b0db5cfcf8032",
-            "title": "[c.c动漫][10月新番][时间飞船24][Time Bokan 24][12][GB][720P][MP4][网盘]",
-            "sort_id": 1,
-            "sort_name": "動畫",
-            "timestamp": 1482336120,
-            "updateusername": "動漫花園鏡像",
-            "updateuserid": 3
-        }';
-
-        $data = json_decode($json,true);
-        
-        $request = [];
-        $request['name'] = $data['title'];
-        $request['hash'] = $data['hash_id'];
-        $request['outlink'] = 'https://share.acgnx.se/show-'.$data['hash_id'].'.html';
-        $request['additional'] = $data['data_id'];
-        $request['token'] = '860F3ABB7EB7E30FAD15EEEF6BA6A07D3386AB8A';
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "http://c.baka/api/add");
-        curl_setopt($ch, CURLOPT_HEADER, false);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 7);
-		curl_setopt($ch, CURLOPT_POST, 1);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($request));
-        $json = curl_exec($ch);
-        echo $json;
-        curl_close($ch);
-
-        
-        
-
-    }
+    
 
     function pull(){
 
