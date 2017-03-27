52614bdd6dfd501a22a80957c2ff3e1f408310dd
uccu <DuanYaFei@jyliMac.local>
2017-03-27 14:21:27 +0800

    添加git同步代码

---
+++ App/School/Controller/GitController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Project\Controller;
+
+
+use AJAX;
+use Controller;
+
+
+class GitController extends Controller{
+
+
+    function __construct(){
+
+        
+
+    }
+
+
+    /* 通过用GIT进行代码同步 */
+    function pull(){
+
+        system("cd ".BASE_ROOT." && \"C:\Program Files\Git\git-cmd.exe\" git pull");
+
+
+    }
+
+
+    
+
+
+}
