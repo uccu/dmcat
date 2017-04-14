77d50c5307b6aadc1aafc886c9c0aae4c213bad7
uccu <DuanYaFei@jyliMac.local>
2017-04-14 13:26:10 +0800

    w

--- View/App/recruit/testprepay.php
+++ View/App/recruit/testprepay.php
@@ -23,7 +23,7 @@ function onBridgeReady(){
                 if(res.err_msg == "get_brand_wcpay_request:ok" ) {
                     alert('1');
                 }else{
-                    alert('3');
+                    alert(res.err_msg);
                 }     // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。 
             }
         ); 
