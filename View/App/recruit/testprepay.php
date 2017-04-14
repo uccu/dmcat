84f9f81b7351e38e0f828a2220d8ca4d5d8f9ec3
uccu <DuanYaFei@jyliMac.local>
2017-04-14 13:08:33 +0800

    wd

--- View/App/recruit/testprepay.php
+++ View/App/recruit/testprepay.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-<script src="js/jquery-1.8.3.min.js"></script>
+<script src="/js/jquery.min.js"></script>
 </head>
 <body>
 
@@ -19,7 +19,7 @@ function onBridgeReady(){
                 if(res.err_msg == "get_brand_wcpay_request:ok" ) {}     // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。 
             }
         ); 
-    })
+    },'json')
    
 }
 if (typeof WeixinJSBridge == "undefined"){
