f58483554d075c8a7f017bbaee80ec6ad6799043
uccu <DuanYaFei@jyliMac.local>
2017-04-21 17:59:09 +0800

    upd

--- App/School/Controller/StudentController.php
+++ App/School/Controller/StudentController.php
@@ -257,7 +257,8 @@ class StudentController extends Controller{
         $where['month'] = date('Ym');
         $where['day'] = date('d');
         
-        !$model->where($where)->find() && AJAX::error('Not Attended');
+        !($z = $model->where($where)->find()) && AJAX::error('Not Attended');
+        $z->leave_time && AJAX::error('Has Left');
 
         $data['update_time'] = TIME_NOW;
         $data['leave_time'] = date('H:i:s');
