5a6eec233073afc9502cc0b363a68adc6f2c4521
uccu <DuanYaFei@jyliMac.local>
2017-04-26 12:53:32 +0800

    rrg

--- App/School/Controller/ParentController.php
+++ App/School/Controller/ParentController.php
@@ -7,6 +7,7 @@ use Controller;
 use Response;
 use App\School\Model\UserModel;
 use App\School\Model\StudentModel;
+use App\School\Model\UserStudentModel;
 use App\School\Middleware\L;
 use App\School\Tool\Func;
 use App\School\Tool\AJAX;
@@ -30,6 +31,26 @@ class ParentController extends Controller{
     function index(){
 
         View::hamlReader('parent/'.__FUNCTION__,'App');
+    }
+
+    function get_my_info($id ,UserModel $model){
+
+        !$id && AJAX::success(['info'=>[]]);
+        $info = $model->select('type','avatar','raw_password>password','phone','email')->find($id);
+        if(!$info)AJAX::error('用户不存在/Not Exist User');
+        if($info->type != '1')AJAX::error('用户非家长/Not Parent');
+
+        
+        if($info->avatar)$info->avatar = Func::fullPicAddr($info->avatar);
+        
+        $out['info'] = $info;
+
+        $out['student'] = UserStudentModel::getInstance()->select('id','studentInfo.name','studentInfo.name_en')->where(['user_id'=>$id])->get()->toArray();
+        !$info && AJAX::error_i18n('no_data');
+        $out['lang'] = $this->lang->language;
+
+        AJAX::success($out);
+
     }
     
 
