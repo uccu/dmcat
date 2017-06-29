5b8025c92ef0584c78d5f94855796f501dbbdb2d
uccu <DuanYaFei@jyliMac.local>
2017-06-29 16:14:41 +0800

    ww

--- View/App/Common/index_top.php
+++ View/App/Common/index_top.php
@@ -1,7 +1,7 @@
 <div id="index_top" class="w-1200">
     <?php echo lang('德汇集团旗下网站');?>：
     <a href="javascript:void(0);"><?php echo lang('德汇集团');?></a> <em>|</em>
-    <a href="javascript:void(0);"><?php echo lang('德汇教育');?></a> <em>|</em>
+    <a href="http://www.doowinedu.com/"><?php echo lang('德汇教育');?></a> <em>|</em>
     <a href="javascript:void(0);"><?php echo lang('德汇金融');?></a> <em></em> <em></em> <em></em>
     <a href="/Home/CountUs/recruit"><?php echo lang('加入我们');?></a> <em>|</em>
     <a href="/Home/CountUs/complaints"><?php echo lang('联系我们');?></a>
