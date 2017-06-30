960d221b0bb881cfdd86c03c6cd4fc9f014ec0b1
uccu <DuanYaFei@jyliMac.local>
2017-06-30 16:51:24 +0800

    w

--- View/App/EnteredDorsey_chairman.php
+++ View/App/EnteredDorsey_chairman.php
@@ -25,7 +25,7 @@
                         echo '<div class="chairman_box">
                     <div class="table">
                         <div class="table-cell">
-                            <img src="/pic/'.$v->first.'">
+                            <img src="/pic/'.$v->first.'" style="height:100%">
                         </div>
                     </div>
                     <span data-image="'.$v->picArray.'">'.$v->count.' 张</span>
