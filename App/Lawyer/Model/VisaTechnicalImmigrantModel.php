2f27e3341159ae9134b1fa124c2365e3f7c8524b
uccu <DuanYaFei@jyliMac.local>
2017-08-07 17:59:02 +0800

    ec

---
+++ App/Lawyer/Model/VisaTechnicalImmigrantModel.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Lawyer\Model;
+
+use Model;
+
+
+class VisaTechnicalImmigrantModel extends Model{
+
+    public $table = 'visa_technical_immigrant';
+    public $field;
+
+
+}
