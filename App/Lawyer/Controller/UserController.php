b4733a2e1fa2c071996152f858187747a42a2b20
company <mbmbn123@hotmail.com>
2017-08-17 10:54:37 +0800

    upd

--- App/Lawyer/Controller/UserController.php
+++ App/Lawyer/Controller/UserController.php
@@ -381,6 +381,16 @@ class UserController extends Controller{
 
     }
 
+    function getStatus(){
+
+        !$this->L->id && AJAX::error('未登录');
+
+        $out['message'] = MessageModel::copyMutiInstance()->where(['user_id'=>$info->id,'isread'=>0])->find() ?'1':'0';
+        $out['consult'] = ConsultModel::copyMutiInstance()->where(['user_id'=>$info->id,'which'=>1,'isread'=>0])->find() ?'1':'0';
+
+        AJAX::success($out);
+    }
+
 
     /** 修改用户名
      * change_name
