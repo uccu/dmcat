e82abf25a09be223fe10783abb51eef0ea19d00c
uccu <418667631@qq.com>
2017-09-29 14:42:53 +0800

    dw

--- App/Car/Controller/UserController.php
+++ App/Car/Controller/UserController.php
@@ -329,7 +329,7 @@ class UserController extends Controller{
 
                 $v->orderInfo->create_date = Func::time_calculate($v->orderInfo->create_time);
                 if($v->driver_id){
-                    $v->driverInfo = UserModel::copyMutiInstance()->select('avatar','name','sex','phone','judge_score','car_number','brand')->find($v->driver_id);
+                    $v->driverInfo = DriverModel::copyMutiInstance()->select('avatar','name','sex','phone','judge_score','car_number','brand')->find($v->driver_id);
                     if(!$v->driverInfo)$v->driver_id = '0';
                     else{
                         $v->driverInfo->order_count = TripModel::copyMutiInstance()->select('COUNT(*) AS c','RAW')->where('type<2')->where(['driver_id'=>$v->driver_id])->find()->c;
