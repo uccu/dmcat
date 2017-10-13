7fc8662699250dfd3f8b245686ea7b44a9f6a968
company <mbmbn123@hotmail.com>
2017-10-13 17:58:44 +0800

    w

--- ttt.js
+++ ttt.js
@@ -29,29 +29,57 @@
 
 
 
-/** 全排列
- * pi
- * @return mixed 
- */
-function pi(a = []){
+// /** 全排列
+//  * pi
+//  * @return mixed 
+//  */
+// function pi(a = []){
 
-    if(!a.length)return [[]]
+//     if(!a.length)return [[]]
 
-    let t = []
+//     let t = []
 
-    for(let i = 0;i<a.length;i++){
+//     for(let i = 0;i<a.length;i++){
 
-        let p = pi(a.slice(0,i).concat(a.slice(i+1)))
+//         let p = pi(a.slice(0,i).concat(a.slice(i+1)))
         
-        for(let j in p){
-            p[j].unshift(a[i])
-        }
-        t = t.concat(p)
-    }
+//         for(let j in p){
+//             p[j].unshift(a[i])
+//         }
+//         t = t.concat(p)
+//     }
 
-    return t
+//     return t
+
+// }
+
+
+// console.log(pi([1,2,3,4,5]))
+
+
+
+/** 最长公共子序列 */
+function z(a = '',b = ''){
+
+    if(a == '' || b == '')return '';
+    
+    var g = '';
+    for(var i = 0;i<a.length;i++){
+
+        var f = a.substr(i,1),e = b.search(f);
+        if(e === -1)continue;
+
+        var u = f + z( a.substr( i+1 ),b.substr( e+1 ) );
+        if( u.length > g.length )g = u;
+       
+    }
+    return g
 
 }
 
+var w = z('wwwqqabwcd','dawbfcqdc');
 
-console.log(pi([1,2,3,4,5]))
+console.log(w)
+
+
+
