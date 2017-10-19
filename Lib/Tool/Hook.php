d638e9109bdbe12793c0bc561c622a0477428635
company <mbmbn123@hotmail.com>
2017-10-19 16:47:38 +0800

    错误修正

--- Lib/Tool/Hook.php
+++ Lib/Tool/Hook.php
@@ -4,7 +4,7 @@ namespace Lib\Tool;
 interface Hook{
 
     # ajax 回调
-    # static function ajaxCallback($code = 0,$data = [],$message = '',$url = '');
+    static function ajaxCallback($code = 0,$data = [],$message = '',$url = '');
 
 
 
