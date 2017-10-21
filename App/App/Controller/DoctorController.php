a887bfc619f736abfaa424fbca1abccd73cb4f37
company <mbmbn123@hotmail.com>
2017-10-21 14:11:00 +0800

    up

---
+++ App/App/Controller/DoctorController.php
@@ -0,0 +1,347 @@
+<?php
+
+namespace App\App\Controller;
+
+use Controller;
+use DB;
+use stdClass;
+use Response;
+use Request;
+use App\App\Middleware\L2;
+use App\App\Tool\Func;
+use Uccu\DmcatTool\Tool\AJAX;
+
+# 数据模型
+use App\App\Model\UserModel;
+use App\App\Model\MessageModel;
+use App\App\Model\UserDateModel;
+use App\App\Model\DoctorModel;
+use Model; 
+
+
+class DoctorController extends Controller{
+
+
+    function __construct(){
+
+        $this->L = L2::getSingleInstance();
+        $this->salt = $this->L->config->SITE_SALT;
+
+    }
+
+    /** 给密码加密
+     * 2333
+     * @param mixed $password 
+     * @param mixed $salt 
+     * @return mixed 
+     */
+    public function encrypt_password($password,$salt){
+        return sha1($this->salt.md5($password).$salt);
+    }
+    /** 生成登录TOKEN
+     * 
+     * @param mixed $info 
+     * @return mixed 
+     */
+    private function encrypt_token($info){
+        return Func::randWord().Func::aes_encode(Func::randWord().base64_encode(sha1($info->password.$this->salt.TIME_NOW).'|'.$info->id.'|'.TIME_NOW));
+    }
+
+    /** 添加新用户
+     * _add_doctor
+     * @param mixed $info 
+     * @return mixed 
+     */
+    private function _add_doctor($info){
+
+        $info->create_time = TIME_NOW;
+        $model = DoctorModel::copyMutiInstance();
+        $model->where(['phone'=>$info->phone])->find() && AJAX::error('手机号已存在');
+        
+
+        DB::start();
+
+
+        $info->id = $model->set($info)->add()->getStatus();
+        !$info->id && AJAX::error('新用户创建失败');
+        
+        $info = $model->find($info->id);
+        $info->name = '用户'.Func::add_zero($info->id,6);
+        $info->avatar = 'noavatar.png';
+        $info->save();
+
+        DB::commit();
+        
+        $this->_out_info($info);
+    }
+
+    /** 登出
+     * 
+     * @return mixed 
+     */
+    function logout(){
+        Response::getSingleInstance()->cookie('doctor_token','',-3600);
+        AJAX::success();
+    }
+
+    /** 登录
+     * 
+     * @param mixed $phone 
+     * @param mixed $password 
+     * @param mixed $cookie 
+     * @return mixed 
+     */
+    function login($type = 0,$code = '',$phone = null,$password =null,DoctorModel $model,$cookie = null){
+
+
+        //检查参数是否存在
+        !$phone && AJAX::error('账号不能为空！');
+        !$password && AJAX::error('密码不能为空！');
+        
+        //找到对应手机号的用户
+        $info = $model->where('phone=%n',$phone)->find();
+        !$info && AJAX::error('用户不存在');
+
+        //是否储存登录信息到cookie
+        if($cookie)$this->cookie = true;
+
+        # 验证密码 加密算法采用  sha1(网站干扰码+md5(密码)+用户干扰码)
+        $encryptedPassword = $this->encrypt_password($password,$info->salt);
+        if($encryptedPassword!=$info->password)AJAX::error('密码错误');
+
+        !$info->active && AJAX::error('账号已被禁用，请联系管理员！');
+
+        if($type == 1){
+            
+            !$code && AJAX::error('无法识别第三方标示！');
+            $model->where(['qq'=>$code])->find() && AJAX::error('已绑定账号，请勿重复绑定！');
+            $info->qq = $code;
+        }elseif($type == 2){
+
+            !$code && AJAX::error('无法识别第三方标示！');
+            $model->where(['wx'=>$code])->find() && AJAX::error('已绑定账号，请勿重复绑定！');
+            $info->wx = $code;
+        }
+
+        $info->save();
+        
+        //输出登录返回信息
+        $this->_out_info($info);
+
+
+    }
+
+    
+
+    /** 输出用户登录信息
+     * 
+     * @param mixed $info 
+     * @return mixed 
+     */
+    private function _out_info($info){
+        
+        $doctor_token = $this->encrypt_token($info);
+        $this->cookie && Response::getSingleInstance()->cookie('doctor_token',$doctor_token,0);
+
+        $out = [
+            'doctor_token'=>$doctor_token,
+            'id'=>$info->id,
+            'avatar'=>$info->avatar,
+            'name'=>$info->name,
+            'type'=>$info->type,
+            
+        ];
+        
+        AJAX::success($out);
+    }
+
+
+    /** 注册
+     * register
+     * @param mixed $password 
+     * @param mixed $phone 
+     * @param mixed $phone_captcha 
+     * @param mixed $cookie 
+     * @return mixed 
+     */
+    function register($type = 0,$code = '',$terminal = 0,DoctorModel $model,$password = null,$phone = null,$phone_captcha,$cookie = false,$parent_id = 0){
+        
+        
+        //是否储存登录信息到cookie
+        if($cookie)$this->cookie = true;
+
+        Func::check_phone($phone);
+        Func::check_password($password);
+        Func::check_phone_captcha($phone,$phone_captcha);
+
+        $info = new stdClass;
+
+        if($id){
+            $parent = $model->find($parent_id);
+            !$parent && AJAX::error('推荐人不存在！');
+            $info->parent_id = $parent_id;
+        }
+        $info->phone        = $phone;
+        $info->terminal     = floor($terminal);
+        $info->salt         = Func::randWord(6);
+        $info->password     = $this->encrypt_password($password,$info->salt);
+
+        if($type == 1){
+            
+            !$code && AJAX::error('无法识别第三方标示！');
+            $model->where(['qq'=>$code])->find() && AJAX::error('已绑定账号，请勿重复绑定！');
+            $info->qq = $code;
+        }elseif($type == 2){
+
+            !$code && AJAX::error('无法识别第三方标示！');
+            $model->where(['wx'=>$code])->find() && AJAX::error('已绑定账号，请勿重复绑定！');
+            $info->wx = $code;
+        }
+
+        $this->_add_doctor($info);
+    }
+
+    
+
+
+
+    /** 修改密码
+     * forget_password
+     * @param mixed $new_password 
+     * @param mixed $phone 
+     * @param mixed $phone_captcha 
+     * @param mixed $cookie 
+     * @return mixed 
+     */
+    function forget_password($new_password,$phone,$phone_captcha,$cookie = false){
+
+        //是否储存登录信息到cookie
+        if($cookie)$this->cookie = true;
+
+        Func::check_phone_captcha($phone,$phone_captcha);
+
+        $model = DoctorModel::copyMutiInstance();
+        if(!$doctorInfo = $model->where(['phone'=>$phone])->find()){
+
+            AJAX::error('用户不存在！');
+        }
+
+        $doctorInfo->password = $this->encrypt_password($new_password,$doctorInfo->salt);
+        $doctorInfo->save();
+
+        $this->_out_info($doctorInfo);
+
+
+    }
+
+    
+    /** 发送手机验证码
+     * captcha
+     * @param mixed $phone 手机号
+     * @param mixed $out 是否输出AJAX
+     * @return mixed 
+     */
+    function captcha($phone,$out = 1) {
+
+        Func::check_phone($phone);
+        Func::msm($phone,$type);
+        if($out)AJAX::success();
+
+    }
+
+
+
+    # 我的信息
+    function getMyInfo(){
+
+        !$this->L->id && AJAX::error('未登录');
+
+        $info = $this->L->doctorInfo;
+
+        unset($info->password);
+        unset($info->salt);
+
+
+        $out['info'] = $info;
+
+        AJAX::success($out);
+    }
+
+    # 修改我的信息
+    function changeMyInfo($name,$avatar,$age,$introduce){
+
+        !$this->L->id && AJAX::error('未登录');
+
+        $name && $this->L->doctorInfo->name = $name;
+        $introduce && $this->L->doctorInfo->introduce = $introduce;
+        $avatar && $this->L->doctorInfo->avatar = $avatar;
+        $age && $this->L->doctorInfo->age = $age;
+
+        $this->L->doctorInfo->save();
+
+        AJAX::success();
+    }
+
+    # 修改头像
+    function changeMyAvatar(){
+
+        !$this->L->id && AJAX::error('未登录');
+
+        $out['path'] = $path = Func::uploadFiles('avatar',100,100);
+        !$path && AJAX::error('上传失败，没有找到上传文件！');
+        
+        $this->L->doctorInfo->avatar = $path;
+        $this->L->doctorInfo->save();
+
+        AJAX::success($out);
+
+    }
+
+    
+
+    /** 意见反馈
+     * feedback
+     * @param mixed $content 反馈内容
+     * @return mixed 
+     */
+    function feedback($content,DoctorFeedbackModel $model){
+
+        !$this->L->id && AJAX::error('未登录');
+        !$content && AJAX::error('内容不能为空！');
+
+        $model->set(['doctor_id'=>$this->L->id,'content'=>$content,'create_time'=>TIME_NOW])->add();
+
+        AJAX::success();
+
+    }
+
+
+    
+
+    /** 评价
+     * judge
+     * @param mixed $doctorDateModel 
+     * @param mixed $id 
+     * @param mixed $comment 
+     * @param mixed $star 
+     * @return mixed 
+     */
+    function judge(UserDateModel $doctorDateModel,$id,$comment,$star){
+
+        !$this->L->id && AJAX::error('未登录');
+
+        !$comment && AJAX::error('评论不能为空！');
+        (!$star || $star<0 || $star > 5) && AJAX::error('评星错误！');
+
+        $info = $doctorDateModel->find($id);
+        $info->status != 3 && AJAX::error('该订单还不能评价！');
+
+        $info->comment = $comment;
+        $info->star = $star;
+        $info->save();
+
+        AJAX::success();
+
+
+    }
+}
---
+++ App/App/Controller/ShopController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\App\Controller;
+
+use Controller;
+use DB;
+use stdClass;
+use Response;
+use Request;
+use App\App\Middleware\L;
+use App\App\Tool\Func;
+use Uccu\DmcatTool\Tool\AJAX;
+use View;
+
+# 数据模型
+use App\App\Model\ShopModel;
+use Model; 
+
+
+class ShopController extends Controller{
+
+
+    function __construct(){
+
+
+    }
+
+
+    /** 商品列表
+     * list
+     * @param mixed $page 
+     * @param mixed $limit 
+     * @param mixed $shopModel 
+     * @return mixed 
+     */
+    function list($page = 1,$limit = 10,ShopModel $shopModel){
+        
+        $where['active'] = 1;
+        $list = $shopModel->selectExcept('detail','param')->where($where)->page($page,$limit)->get()->toArray();
+
+        $out['list'] = $list;
+        AJAX::success($out);
+    }
+
+    /** 商品详情
+     * info
+     * @param mixed $id 
+     * @param mixed $shopModel 
+     * @return mixed 
+     */
+    function info($id,ShopModel $shopModel){
+
+        $info = $shopModel->find($id);
+
+        !$info && AJAX::error('商品不存在！');
+
+        $out['info'] = $info;
+        AJAX::success($out);
+
+    }
+
+    /** 商品详情H5
+     * detail
+     * @param mixed $id 
+     * @param mixed $shopModel 
+     * @return mixed 
+     */
+    function detail($id,ShopModel $shopModel){
+
+        $info = $shopModel->find($id);
+
+        !$info && AJAX::error('商品不存在！');
+
+        View::addData(['title'=>$info->name,'content'=>$info->detail]);
+        View::hamlReader('static','App');
+
+
+    }
+}
