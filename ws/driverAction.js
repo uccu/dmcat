4bf4ddca5cbc265b175126c3cc5b81f501f6666b
company <mbmbn123@hotmail.com>
2017-11-02 12:50:57 +0800

    ww

--- ws/driverAction.js
+++ ws/driverAction.js
@@ -11,7 +11,7 @@ dis = function (lat1, lng1, lat2, lng2) {
     var b = lng1 * Math.PI / 180.0 - lng2 * Math.PI / 180.0;
     var s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2) + Math.cos(radLat1) * Math.cos(radLat2) * Math.pow(Math.sin(b / 2), 2)));
     s = s * 6378.137;
-    s = Math.round(s * 10000) / 10000;
+    s = Math.round(s * 10000) / 10;
     return s
 };
 z = function(obj,con){
