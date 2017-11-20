12de19a5620fa1d7573e8baf57f9f034598ffea3
company <mbmbn123@hotmail.com>
2017-11-20 14:40:22 +0800

    w

--- App/Car/Controller/UserController.php
+++ App/Car/Controller/UserController.php
@@ -1080,7 +1080,9 @@ class UserController extends Controller{
 
     function usedCarList($page = 1,$limit = 10,UsedCarModel $model,AreaModel $areaModel){
 
-        $list = $model->order('create_time desc')->page($page,$limit)->get()->toArray();
+        $where['status'] = 1;
+
+        $list = $model->where($where)->order('create_time desc')->page($page,$limit)->get()->toArray();
 
         foreach($list as &$v){
 
