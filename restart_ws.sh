82725b6ef85c5c549278b89fdbf8e527aeb03dd5
company <mbmbn123@hotmail.com>
2018-01-02 11:20:30 +0800

    upd

--- restart_ws.sh
+++ restart_ws.sh
@@ -3,8 +3,8 @@ ps -ef | grep app.js | grep -v grep
 then
 ps -ef | grep app.js | grep -v grep | cut -c 9-15 | xargs kill -9
 cd /home/app/code/dmcat/ws
-nohup node app.js &
+nohup node app.js >> work.out 2>&1 &
 else
 cd /home/app/code/dmcat/ws
-nohup node app.js &
+nohup node app.js >> work.out 2>&1 &
 fi
