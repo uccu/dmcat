4e40b6aedf9c537822605d9bd741ff8347623a3c
uccu <418667631@qq.com>
2019-06-01 21:26:20 +0800

    upd

--- Public/js/chat2.js
+++ Public/js/chat2.js
@@ -1,44 +1,57 @@
-!function(){
-    var link = 'wss:yoooo.co:7777/',uid,cid,ws;
+!function () {
+    var link = 'ws:127.0.0.1:7777/', uid, cid, ws, master;
     var mesCore = {
-        
-        user_login:function(obj){
-            if(obj.status==200)uid = obj.uid;
+
+        user_login: function (obj) {
+            if (obj.status == 200) uid = obj.uid;
             console.log('登陆成功~');
-            j('.login').addClass('channel').css({height:0,transform:'perspective(1000px) rotateY(90deg)','transform-origin': 'left'});
+            j('.login').addClass('channel').css({ height: 0, transform: 'perspective(1000px) rotateY(90deg)', 'transform-origin': 'left' });
             j('.overlay>.back').removeClass('active');
-            
-            setTimeout(function(){
-                j('.overlay>.back').addClass('active');                
+
+            setTimeout(function () {
+                j('.overlay>.back').addClass('active');
                 j('.login>div').addClass('dn');
                 j('.login>div.channel').removeClass('dn');
-                j('.login').addClass('channel').css({height:200,transform:'scale(2) rotate(-5deg)','transform-origin': 'center'});
-            },650)
+                j('.login').addClass('channel').css({ height: 200, transform: 'scale(2) rotate(-5deg)', 'transform-origin': 'center' });
+            }, 650)
 
 
         },
-        channel_login:function(obj){
-            if(obj.status==200)cid = obj.cid;
+        channel_login: function (obj) {
+            if (obj.status == 200) {
+                cid = obj.cid;
+                master = obj.master;
+            }
             console.log('进入成功~');
             j('.overlay>.back').addClass('dark');
-            j('.login').css({height:0,transform:'perspective(1000px) rotateY(90deg)','transform-origin': 'left'});
-            setTimeout(function(){
+            j('.login').css({ height: 0, transform: 'perspective(1000px) rotateY(90deg)', 'transform-origin': 'left' });
+            setTimeout(function () {
                 j('.login').addClass('dn');
-                j('input.ffff,video,button.ffff').removeClass('dn');
-            },1000)
+                j('video').removeClass('dn');
+                if (master == uid) {
+                    j('input.ffff,button.ffff').removeClass('dn');
+                }else if(obj.url){
+                    play(obj.url);
+                }
+            }, 1000)
         },
-        channel_message:function(obj){
-            console.log(obj.nickname+':'+obj.message);
+        channel_message: function (obj) {
+            if (obj.name == 'playUrl') {
+                play(obj.message);
+            } else {
+                console.log(obj.nickname + ':' + obj.message);
+            }
+
         },
-        video_sync:function(obj){
-            if(obj.uid==uid)return;
-            var time = obj.time+obj.offset;
+        video_sync: function (obj) {
+            if (obj.uid == uid) return;
+            var time = obj.time + obj.offset;
             var v = j('video')[0];
             v.currentTime = time;
-            if(obj.paused && !v.paused){
+            if (obj.paused && !v.paused) {
                 // j('video').unbind('pause');
                 v.pause();
-            }else if(!obj.paused && v.paused){
+            } else if (!obj.paused && v.paused) {
                 //j('video').unbind('play pause');
                 v.play();
                 // j('video').one('playing',function(){
@@ -51,109 +64,108 @@
 
     };
 
-    var connectF = function(c){
-        if(ws)return;
-        
+    var connectF = function (c) {
+        if (ws) return;
+
         ws = new WebSocket(link);
-        window.ws=ws;
-        ws.onopen = d=>{console.log('连接成功~');if(c && typeof c === 'function')c(ws)}
-        ws.onmessage = d=>{
-            try{
+        window.ws = ws;
+        ws.onopen = d => { console.log('连接成功~'); if (c && typeof c === 'function') c(ws) }
+        ws.onmessage = d => {
+            try {
                 var v = JSON.parse(d.data);
-                if(typeof v !=='object')return;
-                if(v.type && typeof mesCore[v.type] ==='function')mesCore[v.type](v);
-            }catch(e){
+                if (typeof v !== 'object') return;
+                if (v.type && typeof mesCore[v.type] === 'function') mesCore[v.type](v);
+            } catch (e) {
                 console.log(e.message);
             }
         };
-        ws.onerror = d=>{ws = undefined;console.log(d)};
-        ws.onclose = d=>{ws = undefined;console.log(d)};
-        ws.sendMessageToChannel = function(x){
-            var obj = {type:'channel_message',message:x,cid:cid};
+        ws.onerror = d => { ws = undefined; console.log(d) };
+        ws.onclose = d => { ws = undefined; console.log(d) };
+        ws.sendMessageToChannel = function (x, name = 'normal') {
+            var obj = { type: 'channel_message', message: x, cid: cid, name: name };
             ws.send(JSON.stringify(obj));
         }
     };
-    j(function(){
-        connectF(function(){
+    j(function () {
+        connectF(function () {
             j('.login').addClass('active');
             j('.overlay>.back').addClass('active');
         });
     })
 
+    j('button.ffff').bind('click', function () {
+        ws.sendMessageToChannel(j('input.ffff').val(), 'playUrl');
+    });
 
-
-    j('.login_a').bind('click',function(){
-        if(!ws){alert('没有连接');return}
-        if(!ws.readyState){alert('未准备就绪');return}
+    j('.login_a').bind('click', function () {
+        if (!ws) { alert('没有连接'); return }
+        if (!ws.readyState) { alert('未准备就绪'); return }
         j(this).unbind('click').addClass('disabled');
-        j('#password,#uid,#nickname,#avatar').attr('disabled','disabled');
-        var g = {type:'user_login',nickname:j('#nickname').val(),password:j('#password').val(),'uid':j('#uid').val(),avatar:j('#avatar').val()};
+        j('#password,#uid,#nickname,#avatar').attr('disabled', 'disabled');
+        var g = { type: 'user_login', nickname: j('#nickname').val(), password: j('#password').val(), 'uid': j('#uid').val(), avatar: j('#avatar').val() };
         ws.send(JSON.stringify(g));
 
 
     });
 
-    j('.channel_a').bind('click',function(){
-        if(!uid){alert('未登录');return}
+    j('.channel_a').bind('click', function () {
+        if (!uid) { alert('未登录'); return }
         j(this).unbind('click').addClass('disabled');
-        j('#channel').attr('disabled','disabled');
-        var g = {type:'channel_login',cid:j('#cid').val()};
+        j('#channel').attr('disabled', 'disabled');
+        var g = { type: 'channel_login', cid: j('#cid').val() };
         ws.send(JSON.stringify(g));
 
 
     });
 
-    j('.info').bind('click',function(){
-        var g = {type:'user_info',uid:uid};
+    j('.info').bind('click', function () {
+        var g = { type: 'user_info', uid: uid };
         ws.send(JSON.stringify(g));
     });
-    
 
-    
-    var sync = function(){
+
+
+    var sync = function () {
         var v = j('video')[0];
-        var g = {type:'video_sync',cid:j('#cid').val(),time:v.currentTime,paused:v.paused?1:0};
+        var g = { type: 'video_sync', cid: j('#cid').val(), time: v.currentTime, paused: v.paused ? 1 : 0 };
         ws.send(JSON.stringify(g));
     };
 
 
-    j('video').bind('play pause',function(e){
+    j('video').bind('play pause', function (e) {
         //console.log(e.target.seeking)
-        if(e.type==='pause' && e.target.seeking)return;
+        if (e.type === 'pause' && e.target.seeking) return;
         sync()
     });
 
     //j('video')[0].controls = true;
-    var play = function(u){
+    var play = function (u) {
         //var f = j('input[type=file]')[0].files[0];
-        if(!u)return;
+        if (!u) return;
         // var u = URL.createObjectURL(f);
         j('video')[0].src = u;
-        j('video').one('canplay',function(){j('video')[0].play();});
-        
+        j('video').one('canplay', function () { j('video')[0].play(); });
+
     };
 
-    j('button.ffff').bind('click',function(){
-        play(j('input.ffff').val())
-    });
 
     j(document).on({
-		dragleave:function(e){e.preventDefault()}, 
-		drop:function(e){e.preventDefault()}, 
-		dragenter:function(e){e.preventDefault()}, 
-		dragover:function(e){e.preventDefault()} 
-	});
+        dragleave: function (e) { e.preventDefault() },
+        drop: function (e) { e.preventDefault() },
+        dragenter: function (e) { e.preventDefault() },
+        dragover: function (e) { e.preventDefault() }
+    });
     j(".overlay").bind({
-        drop:function(e){
+        drop: function (e) {
             e.preventDefault();
             console.log(e);
             play(e.originalEvent.dataTransfer.files[0]);
             j('.overlay>.back').removeClass('drag');
         },
-        dragover:function(e){
-				j('.overlay>.back').addClass('drag');
+        dragover: function (e) {
+            j('.overlay>.back').addClass('drag');
         },
-		dragleave:function(e){
+        dragleave: function (e) {
             j('.overlay>.back').removeClass('drag');
         },
 
