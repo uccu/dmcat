dbcbe76b4a7560920123186de398d7345fcd2193
uccu <418667631@qq.com>
2020-03-17 23:08:30 +0800

    add blog

---
+++ App/Blog/Controller/Api.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Blog\Controller;
+
+use Controller;
+use AJAX;
+
+use App\Blog\Model\BlogModel;
+
+class Api extends Controller
+{
+
+    function menu()
+    {
+        AJAX::success([
+            'list' => [2020, 2019]
+        ]);
+    }
+
+    function getArticleList($page = 1, BlogModel $model, $categoryId = 0)
+    {
+        if ($categoryId) {
+            $model->where(['category_id' => $categoryId]);
+        }
+        $data = $model->page($page, 10)->get()->toArray();
+
+        AJAX::success([
+            'list' => $data
+        ]);
+    }
+
+    function getArticleInfo($id = 0, BlogModel $model)
+    {
+        if (!$id) {
+            AJAX::error('文章不存在！');
+        }
+        $info = $model->find($id);
+
+        if (!$info) {
+            AJAX::error('文章不存在！');
+        }
+
+        AJAX::success([
+            'info' => $info
+        ]);
+    }
+}
---
+++ App/Blog/Model/BlogModel.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Blog\Model;
+
+use Model;
+
+class BlogModel extends Model
+{
+    public $table = 'blog';
+}
