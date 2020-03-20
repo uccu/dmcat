43c14a7c73e6c030a40b2b8b32644e7a089678a8
uccu <mbmbn123@hotmail.com>
2020-03-20 18:36:21 +0800

    visit_log

--- App/Blog/Controller/Api.php
+++ App/Blog/Controller/Api.php
@@ -6,12 +6,16 @@ use Controller;
 use AJAX;
 
 use App\Blog\Model\ArticleModel;
+use App\Blog\Model\ReplyModel;
+use App\Blog\Tool\Smtp;
+use App\Resource\Tool\Func;
 
 class Api extends Controller
 {
 
     function __construct()
     {
+        Func::visit_log();
         header('Access-Control-Allow-Origin:*');
         header('Access-Control-Allow-Methods:POST,GET,OPTIONS');
         header('Access-Control-Allow-Headers:Origin,x-requested-with,content-type,Accept');
@@ -55,6 +59,76 @@ class Api extends Controller
 
         AJAX::success([
             'info' => $info
+        ]);
+    }
+
+    function send()
+    {
+        $email = 'cat@4moe.com';
+        // Smtp::send($email, '评论回复', '<div> <name> 回复了您的评论：</div><div></div>');
+    }
+
+    function reply($name, $comment, $id, $email, ArticleModel $model, ReplyModel $rmodel)
+    {
+        if (!$id) {
+            AJAX::error('文章不存在！');
+        }
+        $info = $model->find($id);
+        if (!$info) {
+            AJAX::error('文章不存在！');
+        }
+        if (!$email) {
+            AJAX::error('请填写email');
+        }
+        if (!$name) {
+            AJAX::error('请填写名字');
+        }
+        if (!$comment) {
+            AJAX::error('请填写内容');
+        }
+
+        $data = $rmodel->where([
+            'article_id' => $id,
+            'name' => $name,
+            'email' => $email,
+            'comment' => $comment
+        ])->find();
+        if ($data) {
+            AJAX::error('请勿重复回复');
+        }
+
+        $id = $rmodel->set([
+            'name' => $name,
+            'email' => $email,
+            'article_id' => $id,
+            'comment' => $comment
+        ])->add();
+
+        if ($id) {
+            Smtp::send('418667631@qq.com', '有个用户评论了您发的文章', '<div> ' . strip_tags($name) . ' 回复了您的文章：<a href="http://blog.yoooo.co/article/' . $info->id . '">' . $info->title . '</a></div>');
+        }
+
+        AJAX::success(['id' => $id]);
+    }
+
+    function getReplyList($id, $page, ReplyModel $model)
+    {
+        if (!$id) {
+            AJAX::error('文章不存在！');
+        }
+        $model->where(['article_id' => $id, 'active' => 1]);
+        $data = $model->select('id', 'name', 'email>avatar', 'comment', 'create_time>createTime')->page($page, 10)->order('create_time desc')->get()->toArray();
+        foreach ($data as $v) {
+            if (!$v->avatar) {
+                $v->avatar = '/pic/myAvatar.jpg';
+                $v->self = 1;
+            } else {
+                $v->avatar = 'https://secure.gravatar.com/avatar/' . md5($v->avatar) . '?s=48';
+            }
+        }
+
+        AJAX::success([
+            'list' => $data
         ]);
     }
 }
